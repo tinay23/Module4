73e9a2750f56e2d12951a26cacdc0c5ce2cc2bb7
tinay23 <2005gvasquez@gmail.com>
2023-04-27 17:17:16 -0400

    Uploading project

---
+++ calendar.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+
+// putn 3 or 4 holidays inside  a array go on a pahe looking on todays date check if they see if thy match and if they amdtch they say they are a holiday and if not is not a holiday adn say thesee are holidays 
+
+>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title> Module 4 </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <!-- calendar -->
+    <div class="container">
+        <h1>Calendar</h1>
+        <hr>
+        <table class="table1">
+
+        </table>
+
+    </div>
+    <!-- string? -->
+       <?php echo ("Yanit Vasquez"); 
+       $numberOfStudentsAtDae = "23";
+       echo ($numberOfStudentsAtDae);
+       ?>
+
+       <?php
+    //    outputs words backwards in a string 
+    echo strrev("Holidays in the month of april");
+    //   name consent 
+    define("ENTRY", "Holiday Calendar");
+    echo ENTRY;
+
+    //    multi-demensional array 
+       $holidays = array (
+        array("April Fools Day", "April 1,2023"),
+        array("Easter", "April 9,2023"),
+        array("Earth Day", "April 22,2023"),
+        array("Cinco de Mayo", "May 5,2023")
+       );
+// if else statement 
+       if ( $holidays == "") {
+
+       } else if (){
+
+       }
+       ?>
+    </body>
+<html>
